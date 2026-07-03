b0b546f84c929348d984d2321a28621852054b24
Rishabh Kumar <kumarrishabh050@gmail.com>
2026-07-03 10:20:11 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import StackNavigation from "./src/routes/StackNavigation";
+
+const App = () => {
+  return (
+    <StackNavigation />
+  );
+};
+
+export default App;
